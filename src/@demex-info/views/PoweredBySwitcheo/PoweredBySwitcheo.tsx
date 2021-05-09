207a1ef89c48278145905b9fa7cd7be113a676df
sarah-thong <sarah.thong@switcheo.network>
2021-05-09 13:27:52 +0800

    Change Powered By Switcheo styles

--- src/@demex-info/views/PoweredBySwitcheo/PoweredBySwitcheo.tsx
+++ src/@demex-info/views/PoweredBySwitcheo/PoweredBySwitcheo.tsx
@@ -2,7 +2,7 @@ import { default as ScrollingTextBold } from "@demex-info/assets/animations/Scro
 import { TypographyLabel, withLightTheme } from "@demex-info/components";
 import { lottieDefaultOptions, StaticLinks } from "@demex-info/constants";
 import {
-  Box, Button, Link, makeStyles, Theme, Typography,
+  Box, Button, Hidden, Link, makeStyles, Theme, Typography,
   useMediaQuery, useTheme,
 } from "@material-ui/core";
 import clsx from "clsx";
@@ -29,7 +29,7 @@ const PoweredBySwitcheo: React.FC = () => {
             { ...lottieDefaultOptions }
             animationData={ScrollingTextBold}
             style={{
-              height: widthSmDown ? "6.8rem" : "8.4rem",
+              height: widthSmDown ? "8.8rem" : "10rem",
             }}
           />
         </Suspense>
@@ -40,7 +40,11 @@ const PoweredBySwitcheo: React.FC = () => {
             className={clsx(classes.slide, classes.tradehubH3, "title", { open: sectionView })}
             variant="h3"
           >
-            Powered by Switcheo TradeHub
+            Powered by&nbsp;
+            <Hidden smDown>
+              <br />
+            </Hidden>
+            Switcheo TradeHub
           </Typography>
         </Box>
         <Box className={classes.rightDiv}>
@@ -89,21 +93,22 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
   },
   leftDiv: {
-    padding: theme.spacing(0, 3, 0, 0),
-    width: "calc(100% / 12 * 4)",
+    padding: theme.spacing(0, 6, 0, 0),
+    maxWidth: "calc(100% / 12 * 5)",
+    width: "100%",
     [theme.breakpoints.only("md")]: {
-      padding: theme.spacing(0, 2, 0, 0),
-      width: "calc(100% / 12 * 5)",
+      padding: theme.spacing(0, 5, 0, 0),
+      maxWidth: "calc(100% / 12 * 4)",
     },
     [theme.breakpoints.down("sm")]: {
       padding: 0,
-      width: "100%",
+      maxWidth: "unset",
     },
   },
   rightDiv: {
-    width: "calc(100% / 12 * 8)",
+    width: "calc(100% / 12 * 7)",
     [theme.breakpoints.only("md")]: {
-      width: "calc(100% / 12 * 7)",
+      width: "calc(100% / 12 * 8)",
     },
     [theme.breakpoints.down("sm")]: {
       width: "100%",
@@ -112,7 +117,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   root: {
     backgroundColor: theme.palette.background.paper,
     color: theme.palette.text.primary,
-    padding: theme.spacing(0, 0, 8),
+    padding: theme.spacing(0, 0, 6.5),
     [theme.breakpoints.down("sm")]: {
       padding: 0,
     },
@@ -124,7 +129,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   tradeHubBox: {
     display: "flex",
-    margin: theme.spacing(8, "auto"),
+    margin: theme.spacing(12.5, "auto"),
     maxWidth: "84rem",
     padding: 0,
     width: `calc(100% - ${theme.spacing(12)}px)`,
@@ -134,14 +139,14 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
     [theme.breakpoints.only("sm")]: {
       display: "block",
-      margin: theme.spacing(5, "auto", 0),
+      margin: theme.spacing(8, "auto", 0),
       maxWidth: "100%",
       width: "unset",
       padding: theme.spacing(0, 5, 9),
     },
     [theme.breakpoints.only("xs")]: {
       display: "block",
-      margin: theme.spacing(5, "auto", 0),
+      margin: theme.spacing(8, "auto", 0),
       maxWidth: "100%",
       width: "unset",
       padding: theme.spacing(0, 4, 8),
