01caaae8d9472ebcbcb21bd70cd305d2f2e2001d
Sharmaine Cheong <88943108+sharmainec@users.noreply.github.com>
2022-09-23 16:21:58 +0800

    Fix features

--- src/@demex-info/views/Features/Features.tsx
+++ src/@demex-info/views/Features/Features.tsx
@@ -69,7 +69,7 @@ const Features: React.FC = () => {
       <Box className={classes.featuresRow}>
         <Cards className={classes.cards}>
           <Box>
-            <TypographyLabel className={classes.cardHeader}>Decentralized Central Limit Orderbooks</TypographyLabel>
+            <TypographyLabel className={clsx(classes.cardHeader, "orderBooks")}>Decentralized Central Limit Orderbooks</TypographyLabel>
             <TypographyLabel className={classes.description}>Enjoy low slippage and flexible trades with guaranteed liquidity from innovative AMM-backed orderbooks.</TypographyLabel>
             <Divider className={classes.gradientDivider} />
             <SvgIcon className={clsx(classes.assets, "orderBook")} component={Orderbooks}/>
@@ -130,11 +130,13 @@ const useStyles = makeStyles((theme) => ({
     padding: "2.5rem",
     "@media (min-width: 1279px) and (max-width: 1429px)": {
       width: "50%",
+      minWidth: "unset",
+      minHeight: "560px",
     },
     [theme.breakpoints.only("md")]: {
       width: "50%",
       minWidth: "unset",
-      minHeight: "435px",
+      minHeight: "470px",
     },
     [theme.breakpoints.down("sm")]: {
       width: "100%",
@@ -161,6 +163,14 @@ const useStyles = makeStyles((theme) => ({
     ...theme.typography.h2,
     color: theme.palette.text.primary,
     marginBottom: "2.5rem",
+    "&.orderBooks": {
+      "@media (min-width: 1279px) and (max-width: 1429px)": {
+        height: "4.75rem",
+      },
+      [theme.breakpoints.only("md")]: {
+        minHeight: "3.5rem",
+      },
+    },
     "&.crosschain": {
       height: "4.75rem",
       [theme.breakpoints.only("md")]: {
@@ -238,6 +248,14 @@ const useStyles = makeStyles((theme) => ({
       },
       "&.noLeftMargin": {
         marginLeft: "-3rem",
+      },
+    },
+    "@media (min-width: 1279px) and (max-width: 1429px)": {
+      width: "100%",
+      height: "100%",
+      minHeight: "2.25rem",
+      "&.orderBook": {
+        marginTop: "2rem",
       },
     },
     [theme.breakpoints.only("md")]: {
