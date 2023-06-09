fff5527dc023fe5cc32a533edd060d2fc81284db
Crystal Cheong <65748007+crystalcheong@users.noreply.github.com>
2023-06-09 21:48:25 +0800

    feat!: updated tokens + sdk

---
+++ src/@demex-info/components/CoinIcon/constants.ts
@@ -0,0 +1,25 @@
+import { importAssets } from "@demex-info/utils";
+import * as COINS from "@demex-info/assets";
+
+export const SymbolToIcon: Record<string, any> = {
+  ...COINS,
+  "1INCH": COINS.INCH,
+};
+
+export const TokenNameMap: Record<string, string> = {
+  IUSD: "USD",
+  NNEO: "NEO",
+  BTCB: "BTC",
+  BCFX: "CFX",
+  "LKT.BEP20": "LKT",
+  LUNC: "LUNA",
+  USTC: "UST",
+  USD: "CUSD",
+  AMPLUNA: "ampLUNA",
+  AMPKUJI: "ampKUJI",
+};
+
+export const TokenAssets = importAssets(
+  // eslint-disable-next-line no-undef
+  require.context("@demex-info/assets/tokens", false, /\.(png|jpe?g|svg)$/),
+);
