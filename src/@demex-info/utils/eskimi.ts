430951def684fdf5072cf07e049a4d34e2a83b31
Steven Khong <khongwh@gmail.com>
2023-07-29 15:59:49 +0700

    Fix eskimi not initializing properly

--- src/@demex-info/utils/eskimi.ts
+++ src/@demex-info/utils/eskimi.ts
@@ -1,3 +1,19 @@
+let init = false;
+const ESKIMI_ID = process.env.ESKIMI_ID;
+
+const initEskimi = () => {
+  if (init || !ESKIMI_ID) return;
+  const eskimi = (window as any).esk;
+  if (typeof eskimi === "function") {
+    init = true;
+    eskimi("init", ESKIMI_ID);
+  } else {
+    setTimeout(initEskimi, 1000);
+  }
+};
+
+initEskimi();
+
 export const eskimi = (...args: any[]) => {
   (window as any).esk?.(...args);
 };
