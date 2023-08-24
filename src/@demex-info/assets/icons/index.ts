05ce6e9851f8695373654a28bb3c80290cb360fe
Thong Yuan Yu Sarah <sarah.thong@switcheo.network>
2023-08-24 11:20:03 +0800

    Add suggestions link to socials bar

--- src/@demex-info/assets/icons/index.ts
+++ src/@demex-info/assets/icons/index.ts
@@ -1,11 +1,12 @@
 export { ReactComponent as AttachMoney } from "./AttachMoney.svg";
-export { ReactComponent as CloseIcon } from "./Cross.svg";
 export { ReactComponent as CosmosNetwork } from "./CosmosNetwork.svg";
+export { ReactComponent as CloseIcon } from "./Cross.svg";
 export { ReactComponent as ExternalLink } from "./ExternalLink.svg";
 export { ReactComponent as InternetOfBlockchains } from "./InternetOfBlockchains.svg";
 export { ReactComponent as MenuIcon } from "./Menu.svg";
 export { ReactComponent as Nitron } from "./Nitron.svg";
+export { ReactComponent as OSMOGradient } from "./OSMOGradient.svg";
 export { ReactComponent as Placeholder } from "./Placeholder.svg";
 export { ReactComponent as TendermintCore } from "./TendermintCore.svg";
 export { ReactComponent as TickIcon } from "./Tick.svg";
-export { ReactComponent as OSMOGradient } from "./OSMOGradient.svg";
+export { ReactComponent as TipsIcon } from "./Tips.svg";
