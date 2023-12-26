1fcb696cdb2813066aa53483547275fe1a766b84
Thong Yuan Yu Sarah <sarah.thong@switcheo.network>
2023-12-26 11:57:45 +0800

    Remove extra interval in TextLoop array

--- src/@demex-info/views/DexProperties/DexProperties.tsx
+++ src/@demex-info/views/DexProperties/DexProperties.tsx
@@ -21,7 +21,7 @@ const DexProperties: React.FC = () => {
           <TypographyLabel boxClass={classes.typoContainer} className={classes.typography}>This is&nbsp;
           <TextLoop
           mask={true}
-					interval={[1000, 1000, 1000, 1000, 1000, 1000, 1000, 2000]}
+					interval={[1000, 1000, 1000, 1000, 1000, 1000, 1000]}
 				>
 					{items.map((item: string) => (
 						<span key={`${item}`}>{item}</span>
